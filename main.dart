fc97f294fd403286f66beb79a131b452bc061925
finteger <104397474+finteger@users.noreply.github.com>
2024-11-28 15:21:08 -0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -14,7 +14,7 @@ class MainApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => HomeScreen(),
-        'second': (context) => SecondScreen(),
+        '/second': (context) => SecondScreen(),
       },
     );
   }
@@ -47,6 +47,19 @@ class SecondScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Screen'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Home'),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
   }
 }
